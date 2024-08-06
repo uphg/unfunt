a40807ab2a27217bcb4fcde78305ee22bcc4b3cb
lvheng <1834805770@qq.com>
2024-08-06 17:44:08 +0800

    feat: remove flatMapDeep and pick, omit base function.

--- src/internal/baseOmit.ts
+++
@@ -1,8 +0,0 @@
-import baseOmitBy from './baseOmitBy'
-import type { Key } from './interfaces'
-
-function baseOmit(object: any, excludes: Key[]) {
-  return baseOmitBy(object, (_value, key) => excludes.includes(key))
-}
-
-export default baseOmit
--- src/internal/basePick.ts
+++
@@ -1,9 +0,0 @@
-import hasOwn from '../hasOwn'
-import basePickBy from './basePickBy'
-import type { Key } from './interfaces'
-
-function basePick(obj: unknown, keys: Key[]) {
-  return basePickBy(obj, keys, (_value, key) => hasOwn(obj, key))
-}
-
-export default basePick
